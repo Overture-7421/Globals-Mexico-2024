e5b8184a9bfe7ec792f0adcb550b58e2c4483570
Vanessa_Carranza <v.carranza.carri@gmail.com>
2024-07-11 12:35:44 -0600

    hicimos el gamepad y todo asi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/MoveChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/MoveChassis.java
@@ -7,75 +7,31 @@ la carpeta de "Subsystems". Nota como existe una diferencia entre "package" e "i
 package org.firstinspires.ftc.teamcode.Commands;
 
 
-/*
-Es importante que para que tu Comando funcione importes todas las librerías necesarias
-que vayan a ayudar a la ejecución del subsistema o comando. Java y OnBotJava te irá
-avisando cuando necesites importar algo, puesto que te indicará que falta una "librería".
-En este primer ejemplo para el desarrollo del subsistema del chassis te daremos las
-librerías necesarias para la ejecución. Nota como tiene una estructura:
-
-import -> indica que vas a importar una librería.
-com.arcrobotics.ftclib.command
-                      .geometry -> indica la ubicación de la librería.
-                      .kinematics
-Finalmente damos el nombre del archivo especifico dentro de la librería.
-*/
-
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
 import org.firstinspires.ftc.teamcode.Utils.JoystickHandler;
 
-
-/*
-Java funciona principalmente a base de Clases; estas clases a parte de ser un archivo
-dentro del programa del robot, es una estructura de Programación Orientada a Objetos
-donde podemos declarar, en este caso, motores, sensores, variables y constantes que
-ayuden a la correcta ejecución del prorgama. A continuación se muestra la correcta
-forma de iniciar una clase de nombre "MoveChassis"; debemos de agregar "extends CommandBase"
-puesto que es un comando ya que nos permiten añadir las opciones, funciones y herramientas
-de los comandos.
-*/
-
 public class MoveChassis extends CommandBase {
 
-          /*El siguiente paso para la realización de los comandos, es declarar las variables y las
-    constantes necesarias para la realización del comando. Estas ya que sólo serán usadas en el comando
-    suelen ser "private final" es decir que se usa exclusivamente en esta clase.
+    private final Chassis chassis;
+    private final Gamepad driverGamepad;
 
-    Ej. private final Chassis chassis;
-     */
+            public MoveChassis(Chassis subsystem, Gamepad driverGamepad){
+                chassis = subsystem;
+                this.driverGamepad = driverGamepad;
+                addRequirements(subsystem);
+            }
 
-    /* Declaración de constantes o variables*/
 
-    /* Posteriormente se tiene que crear el "Constructor" del comando. Un constructor
-     inicializa y configura un objeto de comando con parámetros específicos para controlar el comportamiento
-     del robot. Es importante recordar que un comando puede que ser llamado desde un OpMode o un Autónomo, es
-     por eso que debe ser "public", esto lo hace accesible desde otras partes dentro del código.
 
-     Ej. public MoveShooter(Shooter subsystem, double Voltage) {
-     */
 
-        /* Junto a este paso también es relevante especificarle al sistema que no confunda los objetos con los
-    requerimientos del constructor, es por eso que agregamos "this." a las constantes, para especificar que
-    lo que queremos afectar es al objeto.
 
-    Ej. this.shooter = subsystem
-        this.voltage = voltage
-     */
-
-    /* Al final tenemos que agregar "addRequirements" y al subsistema que queremos que se vea afectado para
-    que los cambios se vean afectados.
-
-    Ej. addRequirements(Shooter);
-     */
 
     @Override
     public void execute(){
-        /* Esta última indicación "execute" nos indica precisamente como se va a ejecutar el comando
-        además de que nos da unas últimas explicaciones sobre como va a ser el output (controlado) del mismo.
-         */
+
         double right = -driverGamepad.right_stick_x;
         double left = -driverGamepad.left_stick_y;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SingleArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SingleArm.java
@@ -27,41 +27,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.Controllers.FRCProfiledPIDController;
 import org.firstinspires.ftc.teamcode.Controllers.FRCTrapezoidProfile;
 
-/*
-Java funciona principalmente a base de Clases; estas clases a parte de ser un archivo
-dentro del programa del robot, es una estructura de Programación Orientada a Objetos
-donde podemos declarar, en este caso, motores, sensores, variables y constantes que
-ayuden a la correcta ejecución del prorgama. A continuación se muestra la correcta
-forma de iniciar una clase de nombre "SingleArm"; debemos de agregar "extends SubsystemBase"
-puesto que es un subsystema ya que nos permiten añadir las opciones, funciones y herramientas
-de los subsistemas.
-*/
-
 public class SingleArm extends SubsystemBase {
+
+    private DcMotorEx rightArm;
+    private DcMotorEx leftArm;
     /* -- MOTOR DECLARATION -- */
-
-    /*
-    Un controlador PID (Proporcional-Integral-Derivativo) es una técnica utilizada para controlar
-    la velocidad y posición de un motor DC de manera precisa y estable. La lógica del PID se basa
-    en tres componentes principales:
-
-    1. Proporcional (P): Esta parte del controlador produce una salida que es proporcional al error
-    actual (la diferencia entre el valor deseado y el valor actual). Si el error es grande, la salida
-    proporcional será grande, lo que permite una corrección rápida.
-
-    2. Integral (I): La parte integral considera la acumulación de errores pasados para eliminar errores
-    residuales que pueden no ser corregidos por la acción proporcional sola. Ayuda a reducir el error
-    a cero con el tiempo.
-
-    3. Derivativo (D): La parte derivativa anticipa errores futuros basándose en la tasa de cambio
-    del error. Proporciona una acción de amortiguación, reduciendo la sobreoscilación y mejorando
-    la estabilidad del sistema.
-
-    En el contexto de un motor DC, el controlador PID ajusta la potencia enviada al motor para alcanzar
-    y mantener una velocidad o posición deseada. La salida del PID (la señal de control) modula la
-    velocidad del motor de manera que minimiza el error entre la velocidad/posición deseada y la real.
-    Esto resulta en un control preciso y suave del movimiento del motor.
-    */
 
     /* -- PID DECLARATION -- */
     private FRCProfiledPIDController armPID;
@@ -76,14 +46,9 @@ public class SingleArm extends SubsystemBase {
     public static final double COUNTS_PER_REV = 288;
     public static final double MOTOR_GEAR_RATIO = 1;
 
-    /*
-    Recordemos que tenemos que tener un offset para el motor para que el PID siempre sepa donde esta
-    ubicado después de un reinicio. Pensemos que es el error que le damos al codigo para corregirse al
-    motor y el encoder.
-    */
-
     /* -- MOTOR OFFSET -- */
-    private double motorOffset = -176;
+    private double rightMotorOffset = -176;
+    private double leftMotorOffset = -176;
 
     /*
     Está función permite al programa asignar y dar sentido al Hardware y donde está ubicado en los IDs del
@@ -91,7 +56,10 @@ public class SingleArm extends SubsystemBase {
     como orientación o inicialización de la odometría y el IMU.
     */
     public SingleArm(HardwareMap hardwareMap) {
-        /* -- MOTOR ID -- */
+        rightArm = (DcMotorEx) hardwareMap.get(DcMotor.class, "rightArm");
+        leftArm = (DcMotorEx) hardwareMap.get(DcMotor.class, "leftArm");
+
+           /* -- MOTOR ID -- */
 
         /* -- PID CONSTRUCTOR -- */
         armPID = new FRCProfiledPIDController(0.0, 0.0, 0.0, new FRCTrapezoidProfile.Constraints(0, 0));
@@ -102,14 +70,21 @@ public class SingleArm extends SubsystemBase {
         */
 
         /* -- SET MOTOR MODE AND ZERO POWER BEHAVIOR -- */
-        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); /* Esta configuración nos indica que el motor
-                                                                está configurado para alcanzar el nivel de velocidad
-                                                                indicado sin la necesidad de un encoder.*/
+        rightArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); /* Esta configuración nos indica que el motor
+                                                              está configurado para alcanzar el nivel de velocidad
+                                                               indicado sin la necesidad de un encoder.*/
 
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); /* Esta configuración nos indica que cuando
+        rightArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); /* Esta configuración nos indica que cuando
                                                                         el motor no reciba ningún tipo de poder
                                                                         va a cambiar su comportamiento a frenarse
                                                                         resistiendo cualquier tipo de fuerza externa.*/
+
+
+        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftArm.setDirection(DcMotorSimple.Direction.REVERSE);
 
         /* -- ARMPID RES -- */
         armPID.reset(getPosition()); //Indica que el PID reinicia el comando "getPosition"
@@ -117,14 +92,15 @@ public class SingleArm extends SubsystemBase {
     }
 
     /* -- RESET ZERO FUNCTION -- */
-    public void resetZero() {
-        motorOffset = motor.getCurrentPosition();
+    public void resetZero() {leftMotorOffset = leftArm.getCurrentPosition();
+        rightMotorOffset = rightArm.getCurrentPosition();
     }
+
 
     /* -- GET POSITION FUNCTION -- */
     public double getPosition() {
-        double currentTicks = motor.getCurrentPosition();
-        double currentPosition = (currentTicks / COUNTS_PER_REV * MOTOR_GEAR_RATIO)  - (motorOffset/360);
+        double currentTicks = leftArm.getCurrentPosition();
+        double currentPosition = (currentTicks / COUNTS_PER_REV * MOTOR_GEAR_RATIO)  - (leftMotorOffset/360);
         return currentPosition;
     }
 
@@ -139,8 +115,9 @@ public class SingleArm extends SubsystemBase {
     /* -- MOTOR OUTPUT AND PERIODIC FUNCTION -- */
     @Override
     public void periodic() {
-        double motorOutput = armPID.calculate(getPosition());
-        motor.setPower(motorOutput);
+        double limitOutput = armPID.calculate(getPosition());
+        leftArm.setPower(limitOutput);
+        rightArm.setPower(limitOutput);
 
     }
 }
